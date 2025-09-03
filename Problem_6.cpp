14ea38c9877f13b27e18793d9c3a7dce35468b3b
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-03 23:37:55 +0530

    constants in CPP

---
+++ Problem_6.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	const int sec=60;//const variable must be initialze when it's declared later initialization is not permitted in cPP
+	cout<<" 20 mins = "<<20*sec<<" seconds"<<endl;
+}
