9ba1644b4a8ec44186f71a75ce30938f3492e7bd
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-03 23:28:54 +0530

    Variables in CPP added

---
+++ Problem_5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int id=101;
+	double price=210.5;
+	char ch='A';
+	string name="akshay";
+	
+	cout<<"ID : "<<id<<endl;
+	cout<<"price : "<<price<<endl;
+	cout<<"id : "<<ch<<endl;
+	cout<<"name : "<<name<<endl;
+	
+	return 0;
+}
