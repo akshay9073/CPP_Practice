c306c372f2019b74f4eaa220b71cbed412cfab85
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-03 23:21:18 +0530

    Types of comments in CPP

---
+++ Problem_4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//this is single line comment in C++;
+	cout<<"Hello world!!"<<endl;
+	/*This is a multiple line commnet anything 
+	between this is ignored by the complier*/
+	
+	cout<<"helow world this is akshay"<<endl;
+	
+	return 0;
+}
