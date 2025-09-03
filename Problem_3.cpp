539da51d1e3722fae34eaa485f90222180278403
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-03 23:15:58 +0530

    Print numbers,strings,new line

---
+++ Problem_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Print Integers : "<<3<<endl;
+	cout<<"Floating Numbers : "<<5.543<<endl;
+	cout<<"String : "<<"Hello "<<"\n";
+	cout<<"Use \t of \t tab"<<endl;
+	return 0;
+}
