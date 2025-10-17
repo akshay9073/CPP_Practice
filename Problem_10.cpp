fe3937b7c9d3e8f900a5d2617652fca8a8fd9116
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-17 23:54:14 +0530

    Operators in CPP

---
+++ Problem_10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	
+	cout<<"Enter the values for a  & b "<<endl;
+	cin>>a>>b;
+	
+	cout<<"a+b="<<a+b<<endl;
+	cout<<"a-b="<<a-b<<endl;
+	cout<<"a*b="<<a*b<<endl;
+	cout<<"a/b="<<a/b<<endl;
+	cout<<"a%b="<<a%b<<endl;
+	
+	
+	
+}
