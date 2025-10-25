3877b8efc36d2000760ba798fdacfa9567e99804
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-25 20:30:20 +0530

    String array in size

---
+++ Problem_21.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string names[5]={"akshay","yash","Rushi","jayesh","nikhil"};
+	
+	for(int i=0;i<5;i++)
+	cout<<names[i]<<"\t"<<endl;
+	
+	return 0;
+}
