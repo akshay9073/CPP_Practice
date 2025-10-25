fa5cc38e01111bb9b85ea9831f00c262a13ccfba
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-25 23:45:29 +0530

    array class

---
+++ Problem_24.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+#include<array>
+
+using namespace std;
+
+int main()
+{
+	array<int,5> myIntArray;
+	
+	cout<<"Enter the data for Array : "<<endl;
+	for(int i=0;i<5;i++)
+	cin>>myIntArray[i];
+	
+	for(int i=0;i<5;i++)
+	cout<<myIntArray[i]<<"\t";
+	
+	return 0;
+}
