7e4034ed4e12750f7f137cafcbfaf800f93809b5
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-25 23:48:52 +0530

    array size() function

--- Problem_24.cpp
+++ Problem_24.cpp
@@ -15,5 +15,7 @@ int main()
 	for(int i=0;i<5;i++)
 	cout<<myIntArray[i]<<"\t";
 	
+	cout<<"Size of Array is : "<<myIntArray.size()<<endl;//size of Array is 5 using size() function.
+	
 	return 0;
 }
