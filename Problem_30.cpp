3943f21147a4c307142e68072be308be71c5943a
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-27 11:19:43 +0530

    vector practice

---
+++ Problem_30.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+int main()
+{
+	vector<string> names;
+	
+	names.push_back("King");
+	names.push_back("Hero");
+	names.push_back("Don");
+	names.push_back("Bhai");
+	names.push_back("Bhaiya");
+	
+	cout<<"Before : "<<endl;
+	for(int i=0;i<names.size();i++)
+	cout<<names[i]<<endl;
+	
+	names.insert(names.begin()+2,"Good");
+	names.pop_back();
+	
+	cout<<"After : "<<endl;
+	for(int i=0;i<names.size();i++)
+	cout<<names[i]<<endl;
+	
+}
