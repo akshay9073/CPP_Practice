d916a01bdaf43be1f2d8b48ad2464f912d03f260
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-31 23:25:21 +0530

    stack DSA in CPP

---
+++ Problem_37.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	stack<string>cars;
+	string temp;
+	cout<<"Enter the car names : "<<endl;
+	
+	for(int i=0;i<5;i++)
+	{
+		cin>>temp;
+		cars.push(temp);
+	}
+	
+	
+}
