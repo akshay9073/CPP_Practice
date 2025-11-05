4187f6d507960ebe01f1747118fae397c567cc6e
akshay_9073 <bhandari.akshay29@gmail.com>
2025-11-05 18:36:12 +0530

    value-returning Parameterless

--- Problem_38.cpp
+++ Problem_38.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#define PI 3.14
+
 using namespace std;
 
 int add(int,int);
+float pi();
 
 int main()
 {
@@ -10,6 +13,9 @@ int main()
 	cin>>a>>b;
 	
 	cout<<a<<" + "<<b<<" ="<<add(a,b);
+	cout<<"\nValue of PI is : "<<pi();
+	
+	return 0;
 }
 
 int add(int x,int y)
@@ -17,4 +23,7 @@ int add(int x,int y)
 	return x+y;
 }
 
-
+float pi()
+{
+	return PI;
+}
