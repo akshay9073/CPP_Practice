d111bf28bc12045c5f006d7c23bcb93ee22fb21c
akshay_9073 <bhandari.akshay29@gmail.com>
2025-11-16 10:55:23 +0530

    Recatangle class in cpp

---
+++ Problem_49/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+#include"Rectangle.h"
+
+using namespace std;
+
+int main()
+{
+	Rectangle r1;
+	Rectangle r2(5,2);
+	
+	cout<<"r1 area is "<<r1.area()<<endl;
+	cout<<"r2 area is "<<r2.area()<<endl;
+	
+	r1.setLength(22);
+	r2.setWidth(12);
+	
+	cout<<"r1 length is now "<<r1.getLength()<<endl;
+	cout<<"r1 width is now "<<r1.getWidth()<<endl;
+	
+	cout<<"r1 area is now "<<r1.area()<<endl;
+	
+	return 0;
+}
