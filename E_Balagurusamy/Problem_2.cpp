4cfc0ed559e337ab50bed39af5fe33768283b0fe
akshay_9073 <bhandari.akshay29@gmail.com>
2026-07-31 15:25:54 +0530

    Comments in c++

---
+++ E_Balagurusamy/Problem_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//This is single line comment
+	
+	cout<<"This is comment program"<<endl;
+	
+	cout<<"This can be used anywhere in the line"<<endl;//Comment it shows the meaning of the code or provide information about code
+	
+	/*
+	This is a multiple line comment in c++ when there are more than one line to be commented it can be done
+	
+	*/ 
+}
