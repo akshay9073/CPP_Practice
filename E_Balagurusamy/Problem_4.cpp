12e9b1ab761f4381839ca504b65c3184561a80e4
akshay_9073 <bhandari.akshay29@gmail.com>
2026-07-31 15:48:13 +0530

    Cascading input and output operators

---
+++ E_Balagurusamy/Problem_4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"This is the Cascading operator Example "
+	<<" output and input operator cascading"<<endl;
+	
+	cout<<"Cascding input operator"<<endl;
+	
+	int a,b;
+	cout<<"Input two numbers : "<<endl;
+	cin>>a>>b;
+	
+	cout<<"a : "<<a<<"\t"
+	<<" b : "<<b<<endl;
+	
+	return 0;
+	
+	
+}
