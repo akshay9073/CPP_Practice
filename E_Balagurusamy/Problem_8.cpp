58841a9171f8162474fac18a5d44a1bd1a5b22ee
akshay_9073 <bhandari.akshay29@gmail.com>
2026-07-31 16:11:20 +0530

    ASIC value of char

---
+++ E_Balagurusamy/Problem_8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Enter a character : "<<endl;
+	
+	char var;
+	cin>>var;
+	
+	cout<<"The ASIC value of charcter is : "<<(int)var<<endl;
+	
+	return 0;
+}
