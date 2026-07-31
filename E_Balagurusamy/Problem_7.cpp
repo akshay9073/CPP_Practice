edebd8b26094e9b20bd8da332a004229c5efd8ca
akshay_9073 <bhandari.akshay29@gmail.com>
2026-07-31 16:10:45 +0530

    Greater of the two numbers

---
+++ E_Balagurusamy/Problem_7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	
+	cout<<"Enter the two numbers : "<<endl;
+	cin>>a>>b;
+	
+	if(a>b)
+	cout<<a<<" is greater than "<<b<<endl;
+	else
+	cout<<b<<" is greater than "<<a<<endl;
+	
+	return 0;
+}
