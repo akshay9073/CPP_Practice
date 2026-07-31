7c125400f1247db3c83bb66bad1613a623bcd91f
akshay_9073 <bhandari.akshay29@gmail.com>
2026-07-31 16:14:05 +0530

    Basic arithmetic operations

---
+++ E_Balagurusamy/Problem_9.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c,x;
+	
+	cout<<"Enter the value of a,b,c : "<<endl;
+	cin>>a>>b>>c;
+	
+	x=a/b-c;
+	
+	cout<<"Value of x : "<<x<<endl;
+	
+	return 0;
+}
