d81421bb27106890647e74b1ab005be63a345c51
akshay_9073 <bhandari.akshay29@gmail.com>
2026-07-31 15:38:48 +0530

    Average of Two Numbers

---
+++ E_Balagurusamy/Problem_3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float num1,num2,average;
+	
+	cout<<"Enter the Number 1 : "<<endl;
+	cin>>num1;
+	
+	cout<<"Enter the Number 2 : "<<endl;
+	cin>>num2;
+	average=(num1+num2)/2;
+	
+	cout<<"Average : "<<average<<endl;
+	
+	return 0;
+}
