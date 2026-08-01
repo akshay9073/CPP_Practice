2d231a309d9824e28563ac4b205ee076744f7201
akshay_9073 <bhandari.akshay29@gmail.com>
2026-08-01 20:37:56 +0530

    type casting in cpp

---
+++ E_Balagurusamy/Problem_15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int intvar=25;
+	float floatvar=37.45;
+	
+	cout<<"Int variable : "<<intvar<<endl;
+	cout<<"float variable : "<<floatvar<<endl;
+	
+	cout<<"float(intvar) : "<<float(intvar)<<endl;
+	cout<<"int(floatvar) :"<<int(floatvar)<<endl;
+	
+	
+	return 0;
+}
