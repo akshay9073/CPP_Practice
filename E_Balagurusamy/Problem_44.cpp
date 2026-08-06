68edd15a58385b1236689814f5f1920ab9228064
akshay_9073 <bhandari.akshay29@gmail.com>
2026-08-06 14:23:01 +0530

    Constructor and it's type

---
+++ E_Balagurusamy/Problem_44.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+class complex
+{
+	float x,y;
+	
+	public:
+		complex() {}
+		complex(float a)
+		{
+			x=y=a;
+		}
+		complex(float real, float imag)
+		{
+			x=real;
+			y=imag;
+		}
+		
+		friend complex sum(complex, complex);
+		friend void display(complex);
+};
+
+complex sum(complex c1,complex c2)
+{
+	complex c3;
+	c3.x=c1.x+c2.x;
+	c3.y=c1.y+c2.y;
+	
+	return c3;
+}
+
+void display(complex c)
+{
+	cout<<"Complex number : "<<c.x<<" + "<<" j"<<c.y<<endl;
+}
+
+int main()
+{
+	complex P,Q,R;
+	
+	P=complex(2.5,3.9);
+	Q=complex(1.6,2.5);
+	
+	R=sum(P,Q);
+	
+	cout<<"\n";
+	cout<<"P = "; display(P);
+	cout<<"Q = "; display(Q);
+	cout<<"R = "; display(R);
+	
+	return 0;
+}
