3b49fef28b64a7dbb1e0b2af9d000f75de63cb9c
akshay_9073 <bhandari.akshay29@gmail.com>
2026-08-14 00:25:48 +0530

    Ambugity resolution in cpp

---
+++ E_Balagurusamy/Problem_59.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+class M
+{
+	public:
+		void display();
+};
+
+class N
+{
+	public:
+		void display();
+};
+
+class P: public M, public N
+{
+	public:
+		void display();
+};
+
+void M::display()
+{
+	cout<<" Class M "<<endl;
+}
+
+void N::display()
+{
+	cout<<" Class N "<<endl;
+//	M::display();
+}
+
+void P::display()
+{
+	cout<<" Class P "<<endl;
+	M::display();
+}
+
+int main()
+{
+	P p;
+	p.display();
+}
+
+
